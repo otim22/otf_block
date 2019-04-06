ef1d1f27cbbdd8f5eef82888b988784f886669ad
Fredrick Otim <fredrickot@gmail.com>
2019-04-06 18:50:45 +0300

    adds linting and travisCI integration

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "semi": [2, "always"],
+        "space-before-function-paren": ["error", "never"],
+        "no-trailing-spaces": ["error", { "skipBlankLines": true }],
+        "no-multiple-empty-lines": ["error", { "max": 4, "maxEOF": 2 }],
+    }
+};
